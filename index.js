7204562eaf38464bb082ef236d185749e2d913ca
Fabiano Galastri <fgalastri@yahoo.com.br>
2020-07-09 20:23:46 -0300

    Versão inicial

---
+++ index.js
@@ -0,0 +1,28 @@
+function helloworld() {
+    console.log('Hello')
+}
+
+const saudacao = () => {
+    
+    var data = new Date()
+    return data.getHours() <= 12 ? "bom dia" : data.getHours() <= 18 ? "boa tarde" : "boa noite"
+
+}
+
+const teste = (a,b) => { 
+    const teste2 = () => {
+        console.log( 'teste' )
+      }
+  }
+
+  
+console.log('bonito')
+const soma = (a,b) =>  a + b 
+
+console.log( soma(2,3))
+
+const divisao = function (a,b) { return a/b }
+console.log(divisao(10,2))
+
+const mult = (a,b) => { return a * b }
+console.log(mult(40,5))
